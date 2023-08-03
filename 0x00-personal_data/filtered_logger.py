2ab5468999d5f1ff9b57e61a28d2bccdd6b27abe
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-03 13:17:52 +0100

    Log formatter override

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ Filtered Logger. """
 
 import re
+import logging
 from typing import List
 
 
@@ -14,3 +15,23 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(fr'{field}=[^{separator}]*',
                          f'{field}={redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the message while obfuscating it."""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
