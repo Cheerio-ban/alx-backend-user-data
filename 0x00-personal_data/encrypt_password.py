4cb8646a8caaba328541bf218097817549e9144b
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-04 02:18:04 +0100

    Revamped hashed pass func

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,9 +5,9 @@
 import bcrypt
 
 
-def hash_password(strings: str) -> bytes:
-    """Hashes a password using bcrypt"""
-    return bcrypt.hashpw(strings.encode(), bcrypt.gensalt())
+def hash_password(password: str) -> bytes:
+    """Return the hash of a password."""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
