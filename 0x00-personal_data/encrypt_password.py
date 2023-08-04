fb8da0336aba0ef326e350621e8fb6b630e7f13b
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-04 02:11:18 +0100

    Evaluates password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,8 @@ def hash_password(string: str) -> bytes:
     byter = string.encode('utf-8')
     pwd_hash = bcrypt.hashpw(byter, salt)
     return pwd_hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks password hash"""
+    return bcrypt.checkpw(password, hashed_password)
