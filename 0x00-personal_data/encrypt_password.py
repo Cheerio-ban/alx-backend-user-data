0cc92512e0c78d08cadcac6d9ef860837e3e75ae
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-04 02:12:39 +0100

    Evaluates password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,11 +8,11 @@ import bcrypt
 def hash_password(string: str) -> bytes:
     """Hashes a password using bcrypt"""
     salt = bcrypt.gensalt()
-    byter = string.encode('utf-8')
+    byter = string.encode()
     pwd_hash = bcrypt.hashpw(byter, salt)
     return pwd_hash
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks password hash"""
-    return bcrypt.checkpw(password, hashed_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
