d53462039385f91ffcf096882f60a1ae0645a972
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-04 01:53:28 +0100

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -47,3 +47,34 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(fmt)
     logger.addHandler(handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Return a db connection."""
+    DB_USER = os.environ['PERSONAL_DATA_DB_USERNAME']
+    DB_PASSWORD = os.environ['PERSONAL_DATA_DB_PASSWORD']
+    DB_HOST = os.environ['PERSONAL_DATA_DB_HOST']
+    DB_NAME = os.environ['PERSONAL_DATA_DB_NAME']
+    return mysql.connector.connect(host=DB_HOST, user=DB_USER,
+                                   password=DB_PASSWORD, database=DB_NAME)
+
+
+def main() -> None:
+    """Print user record from db."""
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users;')
+    fmt = RedactingFormatter(PII_FIELDS)
+    for (name, email, phone, ssn,
+         password, ip, last_login, user_agent) in cursor:
+        msg = (
+            f'name={name};email={email};phone={phone};ssn={ssn}'
+            f'password=password;ip={ip};'
+            f'last_login={last_login};user_agent={user_agent};')
+        logger = logging.LogRecord('user_data', logging.INFO, None, None,
+                                   msg, None, None)
+        print(fmt.format(logger))
+
+
+if __name__ == '__main__':
+    main()
