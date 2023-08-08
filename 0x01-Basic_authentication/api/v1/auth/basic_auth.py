44fe1a7f4036b9c8991898af0f6fc396fd387766
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-08 09:19:16 +0100

    Implemented Base 64 decode function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Contains Basic auth
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decodes base64 authorization header """
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64.b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        return base64.b64decode(base_authorization_header).decode()
