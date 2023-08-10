175f7f8b61c7ed1c9ab301052b5f64d8751c25bb
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-10 11:39:36 +0100

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,5 +20,5 @@ class SessionAuth(Auth):
         if not isinstance(user_id, str):
             return
         session_id = str(uuid.uuid4())
-        user_id_by_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
