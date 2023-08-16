304aad3e66ac809a81f76a0b74a634d223c87abe
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-08-16 12:09:38 +0100

    Finds user in database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,3 +39,11 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """ Find users in the database. """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except (NoResultFound, InvalidRequestError):
+            raise
---
+++ 0x03-user_authentication_service/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+def func(**kwargs):
+    print(dict(**kwargs))
+
+
+func(name="Peejay")
